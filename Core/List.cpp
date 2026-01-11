f90f6f0c77eaa45fd43802b1d23164e77a3b38b4
JavidanValiyev <valiyev.javidan@gmail.com>
2026-01-11 16:59:21 +0400

    header and cpp file created for list type

---
+++ Core/List.cpp
@@ -0,0 +1,65 @@
+//
+// Created by cemre on 11.01.2026.
+//
+
+#include "List.h"
+#include <iostream>
+#include <cstring>
+
+List::List() {
+    currentPosition = -1;
+    array = new int[size];
+}
+
+
+void List::Add(const int item) {
+    currentPosition++;
+    if (currentPosition == size) {
+        size = size * 2;
+        const auto newArray = new int[size];
+        memcpy(newArray, array, size * sizeof(int));
+        free(array);
+        array = newArray;
+    }
+    array[currentPosition] = item;
+}
+
+void List::RemoveAt(int const index) {
+    if (index < 0 || index >= currentPosition) throw std::out_of_range("Index out of range");
+    currentPosition--;
+    const int *source = &array[index + 1];
+    int *destination = &array[index];
+    if (const int elementsToMove = currentPosition - index - 1; elementsToMove > 0) {
+        memmove(destination, source, elementsToMove * sizeof(int));
+    }
+    if (static_cast<float>(currentPosition) / static_cast<float>(size) <= 0.2) {
+        this->Shrink();
+    }
+}
+
+int List::Get(const int item) const {
+    if (item > currentPosition)
+        throw std::out_of_range("Index out of range");
+    return array[item];
+}
+
+int &List::operator[](const int item) const {
+    if (item > currentPosition)
+        throw std::out_of_range("Index out of range");
+    return array[item];
+}
+
+
+void List::Shrink() {
+    if (size < 200)
+        return;
+    size = size / 2;
+    const auto newArray = new int[size];
+    memcpy(newArray, array, size * sizeof(int));
+    free(array);
+    array = newArray;
+}
+
+List::~List() {
+    free(array);
+}
---
+++ Core/List.h
@@ -0,0 +1,20 @@
+#ifndef UNTITLED_LIST_H
+#define UNTITLED_LIST_H
+
+struct List {
+private:
+    int size = 100;
+    int *array;
+    int currentPosition;
+    void Shrink();
+public:
+    List();
+    void Add(int item);
+    [[nodiscard]] int Get(int item) const;
+    int &operator[](int item) const;
+    void RemoveAt(int kindex);
+    ~List();
+};
+
+
+#endif //UNTITLED_LIST_H
